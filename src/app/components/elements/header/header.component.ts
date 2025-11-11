8c31f86491f6dce8d11e9073b6a80ae0228a372a
luansantanaduarte <lsantana.duarte7@gmail.com>
2025-11-11 15:12:29 -0300

    style(sidebar): estlizando sidebar

--- src/app/components/elements/header/header.component.ts
+++ src/app/components/elements/header/header.component.ts
@@ -1,11 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  ativo = true;
 
+  mostrarSidebar() {
+    this.ativo = !this.ativo;
+  }
+
+  esconderSidebar() {
+    this.ativo = false;
+  }
 }
